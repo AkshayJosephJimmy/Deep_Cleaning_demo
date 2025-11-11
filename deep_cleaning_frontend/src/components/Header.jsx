9606046a1056f3a612d77ad008aa47024261abdb
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2025-11-11 09:49:41 +0530

    responsive design for header made

--- deep_cleaning_frontend/src/components/Header.jsx
+++ deep_cleaning_frontend/src/components/Header.jsx
@@ -7,19 +7,19 @@ function Header(){
 
 
     return(
-        <div className="w-full h-60 p-4 bg-linear-to-r from-gray-100 to-green-200 flex">
-            <div>
+        <div className="w-full h-60 p-4 bg-linear-to-r from-gray-100 to-green-200 flex md:justify-between items-center ">
+            <div className="flex flex-col gap-2 md:ml-50">
 
-            <h1 className="text-2xl text-black font-bold">Best Deep Cleaning in Kottayam</h1>
+            <h1 className="text-2xl text-black font-bold md:text-5xl">Best Deep Cleaning in Kottayam</h1>
             
-            <p className="text-xs">Why go else where when we have the best proffesionals in the deep cleaning space</p>
-            <button className="text-xs bg-sky-400 p-2 rounded-2xl mt-2">Get started</button>
+            <p className="text-xs md:text-base">Why go else where when we have the best proffesionals in the deep cleaning space</p>
+            <button className="text-xs text-white w-fit bg-sky-400 p-2 rounded-2xl mt-2 md:text-lg md:rounded-4xl">Get started</button>
             </div>
             
                
                 
                 
-            <img src={svgLogo} alt="logo" className="w-40 h-40 "/>
+            <img src={svgLogo} alt="logo" className="w-40 h-40 md:size-100"/>
             
             
         </div>
