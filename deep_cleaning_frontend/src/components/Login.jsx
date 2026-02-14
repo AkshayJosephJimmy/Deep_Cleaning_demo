b7aa8f3c76b2c38e19e97ff874c202083c3b1340
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-02-14 10:57:23 +0530

    frontend login for admin login made

--- deep_cleaning_frontend/src/components/Login.jsx
+++ deep_cleaning_frontend/src/components/Login.jsx
@@ -2,16 +2,37 @@ import GoogleLogin from "./GoogleLogin";
 
 
 function Login() {
+  const [username,setUsername]=useState('');
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    axios.post("http://localhost:5001/auth/login", {
+      username: username,
+      email: email,
+      password: password
+    }).then((response)=>{
+      console.log(response.data);
+      localStorage.setItem("token",response.data.token);
+      console.log(localStorage.getItem("token"));
+      navigate('/booking');  
+    }).catch((error)=>{
+      console.error("Login error:", error);
+      alert("Login failed. Please check your credentials and try again.");
+    });
+  }
   return (
      
       <div className=" w-80 h-120  m-auto bg-gray-200 ">
-        <form className="flex flex-col justify-center items-center p-5">
+        <form className="flex flex-col justify-center items-center p-5" onSubmit={handleSubmit}>
             <h2 className="text-2xl font-bold mb-4">Login</h2>
-            <input type="text" placeholder="Username" className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
-            <input type="email" placeholder="Email" className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
-            <input type="password" placeholder="Password" className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
+            <input type="text" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
+            <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
+            <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} className="w-64 p-2 mb-4 border border-gray-300 rounded"/>
           
-            <button type="submit" className="w-64 p-2 bg-sky-400 text-white mt-3 rounded hover:bg-green-500 transition-all duration-300">Sign In</button>
+            <button type="submit" className="w-64 p-2 bg-sky-400 text-white mt-3 rounded hover:bg-green-500 transition-all duration-300">Login</button>
 
         
 
